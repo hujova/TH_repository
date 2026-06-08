77f36d7f15a4c7fc6cc941168c5be3af81f020b9
TESSIKNOTEBOOK\hujov <hujova.tess@gmail.com>
2026-06-08 05:41:27 +0200

    test s blik letkou rn

---
+++ ard_led_klasic/ard_led_klasic.ino
@@ -0,0 +1,13 @@
+int led=15;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led, HIGH);
+delay(500);
+digitalWrite(led, LOW);
+delay(500);
+}
